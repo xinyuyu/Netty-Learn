bdae8a0990f23709ac8ee9ab9794c1f4b0bac321
xinyuyu <517910752@qq.com>
2018-12-16 16:03:16 +0800

    带客户端的netty通信小例子

---
+++ src/main/java/cn/edu/hust/learn/netty/client/MyServer.java
@@ -0,0 +1,29 @@
+package cn.edu.hust.learn.netty.client;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class MyServer {
+    public static void main(String[] args) throws InterruptedException {
+
+        EventLoopGroup boosGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            serverBootstrap.group(boosGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new MyServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind("localhost", 8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            boosGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+}
